6455498f68913910cc5d5edbca1878fbd89ac6c4
Francisco Gomez granados <pacogomezgranados@gmail.com>
2013-05-15 18:30:41 +0200

    Adding hammer for gesture detection and some CSS.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,54 @@
+$(function() {
+
+    var Touchapulin = function() {
+
+        /**
+         * Dom element where the touch events happen.
+         * @type {Element}
+         */
+        var touchSurface = $(".js-touch-surface");
+
+        /**
+         * Constructor of the app
+         */
+        var init = function() {
+
+            initEvents();
+        };
+
+        /**
+         * Initializes the events
+         */
+        var initEvents = function() {
+
+            touchSurface.hammer().on("touch", touchStarted);
+            touchSurface.hammer().on("release", touchEnded);
+        };
+
+        /**
+         * Called when the touch starts
+         * @param  {Event} event Event generated
+         */
+        var touchStarted = function(event) {
+
+            console.log("touch starts");
+        };
+
+        /**
+         * Called when the touch ends
+         * @param  {Event} event Event generated
+         */
+        var touchEnded = function(event) {
+
+            console.log("touch ends");
+        };
+
+        return {
+            init: init
+        };
+    };
+
+    var app = new Touchapulin();
+    app.init();
+});
 
